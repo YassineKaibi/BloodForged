69389adbf76552e1454e4016d12a682cc863fcbb
ZizyP <kaibi.0yassine@gmail.com>
2025-12-30 23:35:22 +0100

    fixup! configure data generation for item models

--- src/client/java/com/bloodforged/BloodForgedDataGenerator.java
+++ src/client/java/com/bloodforged/BloodForgedDataGenerator.java
@@ -1,11 +1,18 @@
 package com.bloodforged;
 
+import com.bloodforged.datagen.ModItemModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class BloodForgedDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-	}
+        // We'll add providers here as we create them
+        // pack.addProvider(ItemModelProvider::new);
+        // pack.addProvider(RecipeProvider::new);
+        // etc.
+        pack.addProvider(ModItemModelProvider::new);
+    }
 }
